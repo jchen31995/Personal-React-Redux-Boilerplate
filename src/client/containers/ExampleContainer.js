288c588189b5547a77b75d93fb0f18ddf850d4ef
James <jchen31995@gmail.com>
2018-07-28 13:04:49 -0400

    simple reminder to correctly set up react container

--- src/client/containers/ExampleContainer.js
+++ src/client/containers/ExampleContainer.js
@@ -5,6 +5,28 @@ import PropTypes from 'prop-types'
 
 import { exampleAction } from '../redux/example_reducer'
 
+/*
+====================================================================
+  This is how you set up any container component in Redux
+====================================================================
+
+// At top of file: //
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux' 
+
+// At bottom of file: //
+const mapStateToProps = (state) => ({
+   someProp: state.stateInPropToRetrieve 
+})
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+   someDispatchProp: someActionCreator
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+*/
+
 class ExampleContainer extends Component {
   render() {
     this.props.exampleAction()
