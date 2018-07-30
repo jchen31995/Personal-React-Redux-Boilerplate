8993a172eebf2829aab623ee89cd6a52840b8b66
James <jchen31995@gmail.com>
2018-07-30 00:29:10 -0400

    quick .env config file fix

--- src/server/config.js
+++ src/server/config.js
@@ -2,16 +2,15 @@ const dotEnvSafe = require('dotenv-safe')
 const path = require('path')
 
 dotEnvSafe.config({
-  allowEmptyValues: true,
+  allowEmptyValues: false,
   path: path.resolve(__dirname, '..', '../', '.env'),
   example: path.resolve(__dirname, '..', '..', '.env-example'),
 })
 
-const { AUTH_SECRET, PROJECT_NAME } = process.env
+const { PROJECT_NAME } = process.env
 const PORT = process.env.PORT || 3000
 
 module.exports = {
-  AUTH_SECRET,
   PORT,
   PROJECT_NAME,
 }
